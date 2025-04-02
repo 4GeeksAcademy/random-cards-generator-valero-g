36ef7e956dd74ce0b6e6df309336bb2b16ea4d77
valero-g <vgaudo@hotmail.com>
2025-04-02 10:34:32 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,69 @@
-import "bootstrap";
-import "./style.css";
+function generateRandomCard () {
+  let arrPalos = ["heart", "spade", "clubs", "diamond"];
+  let number = Math.floor(Math.random()*13)+1;
+  let formattedNumber = number;
+  switch(number)
+  {
+    case (1):
+      formattedNumber = "AS";
+      break;
+    case(11):
+      formattedNumber = "J";
+      break;
+    case (12):
+      formattedNumber = "Q";
+      break;
+    case (13):
+      formattedNumber = "K";
+      break;
+    default:
+      break;
+  }
+  let palo =  arrPalos[Math.floor(Math.random()*arrPalos.length)];
+
+  console.log(palo);
+  //Seleccionamos palo
+  
+  //let paloInCard= document.getElementsByClassName(palo)[0];
+  //paloInCard.style.display = "flex";
+  document.getElementsByClassName(palo)[0].style.display = "flex";
+  document.getElementsByClassName(palo)[1].style.display = "flex";
+
+  for (let element of arrPalos){
+    if (element != palo){
+      document.getElementsByClassName(element)[0].style.display= "none";
+      document.getElementsByClassName(element)[1].style.display= "none";
+     }
+  }
+  // sobreescribimos número de carta
+  let numberValue = document.getElementsByClassName("numberValue")[0];
+  numberValue.innerHTML = formattedNumber;
+  if (palo== "heart" || palo == "diamond") { 
+   numberValue.style.color = "red";
+  } else numberValue.style.color = "black";
+
+}
+
+let intervalID= 0;
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function periodicRandomCard(){
+  var myCheckbox = document.getElementsByName("timer")[0];
+  var myInputtime = document.getElementsByName("inputTime")[0];
+  clearInterval(intervalID)
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  if (myCheckbox.checked == true){
+    intervalID= setInterval(generateRandomCard,parseInt(myInputtime.value)*1000);
+  } 
+}
+
+function adjustHeight(){
+  var newHeight = document.getElementsByName("inputHeight")[0];
+  document.getElementsByClassName("card")[0].style.height = newHeight.value + "%";
+}
+
+function adjustWidth(){
+  var newWidth = document.getElementsByName("inputWidth")[0];
+  document.getElementsByClassName("card")[0].style.width = newWidth.value + "%";
+}
+
